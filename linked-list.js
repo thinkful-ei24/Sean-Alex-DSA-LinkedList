91b0d194a5cefb964c5d3647b99a3b4fd9fa48d3
Alex <0alexgutierrez@gmail.com>
2018-11-08 13:58:47 -0700

    third from end

--- linked-list.js
+++ linked-list.js
@@ -77,25 +77,23 @@ class LinkedList {
   // key is the value of the node to be matched against
   // newVal is the value of the new element to be inserted
   insertBefore(key, newVal) {
-    if (!this.head)
-      return false;
+    if (!this.head) return false;
 
     let currNode = this.head;
     let previousNode = this.head;
 
-    while(currNode !== null && currNode.value !== key) {
+    while (currNode !== null && currNode.value !== key) {
       previousNode = currNode;
       currNode = currNode.next;
     }
 
-    if (currNode === null)
-      return false;
+    if (currNode === null) return false;
 
     // create our new node that points forward to the target node
     const newNode = new _Node(newVal, currNode);
 
     // if there's no previous node, just set the start node directly
-    if(currNode === previousNode) {
+    if (currNode === previousNode) {
       this.head = newNode;
     } else {
       previousNode.next = newNode;
@@ -106,15 +104,15 @@ class LinkedList {
   // insert x after b: a->b->x->c
   insertAfter(key, newVal) {
     // check if the list is empty
-    if(!this.head) {
+    if (!this.head) {
       return;
     }
 
     let currNode = this.head;
 
-    while(currNode) {
+    while (currNode) {
       // find target node
-      if(currNode.value === key) {
+      if (currNode.value === key) {
         // insert after
         currNode.next = new _Node(newVal, currNode.next);
         return;
@@ -122,11 +120,11 @@ class LinkedList {
       currNode = currNode.next;
     }
   }
-  
+
   // a->b->c->d->e    insert x at index=3
   // a->b->c->x->d->e
   insertAt(index, newValue) {
-    if(index === 0) {
+    if (index === 0) {
       this.insertFirst(newValue);
       return;
     }
@@ -134,9 +132,9 @@ class LinkedList {
     let currNode = this.head;
     let prevNode = this.head;
     let counter = 0;
-    while(currNode) {
+    while (currNode) {
       // find target node
-      if(counter === index) {
+      if (counter === index) {
         prevNode.next = new _Node(newValue, currNode);
       }
       counter++;
@@ -153,7 +151,7 @@ LinkedList.prototype.toString = function() {
 LinkedList.prototype.valueOf = function() {
   const arr = [];
   let currNode = this.head;
-  while(currNode !== null) {
+  while (currNode !== null) {
     arr.push(currNode.value);
     currNode = currNode.next;
   }
@@ -163,7 +161,7 @@ LinkedList.prototype.valueOf = function() {
 function size(linkedList) {
   let currentNode = linkedList.head;
   let i = 0;
-  while(currentNode) {
+  while (currentNode) {
     i++;
     currentNode = currentNode.next;
   }
@@ -177,8 +175,8 @@ function isEmpty(linkedList) {
 function findPrevious(linkedList, targetValue) {
   let currentNode = linkedList.head;
   let prevNode = linkedList.head;
-  while(currentNode) {
-    if(currentNode.value === targetValue) {
+  while (currentNode) {
+    if (currentNode.value === targetValue) {
       return prevNode;
     }
     prevNode = currentNode;
@@ -190,28 +188,86 @@ function findPrevious(linkedList, targetValue) {
 function findLast(linkedList) {
   let currentNode = linkedList.head;
   let prevNode = linkedList.head;
-  while(currentNode) {
+  while (currentNode) {
     prevNode = currentNode;
     currentNode = currentNode.next;
   }
   return prevNode.value;
 }
 
+function whatDoesThisProgramDo(lst) {
+  let current = lst.head;
+  while (current !== null) {
+    let newNode = current;
+    while (newNode.next !== null) {
+      if (newNode.next.value === current.value) {
+        newNode.next = newNode.next.next;
+      } else {
+        newNode = newNode.next;
+      }
+      current = current.next;
+    }
+  }
+}
+
+// loop through list
+// counter++
+// when node.next = null
+// length = counter
+// if current node.next === null, node.next.next === null,
+// node.next.next === null
+
+function third(list) {
+  let current = list.head;
+  while (current) {
+    if (current.next.next.next === null) {
+      return current.value;
+    }
+    current = current.next;
+  }
+  return 'not found';
+}
+
+/**
+ * 1->2->3
+ * 1<-2<-3 or 3->2->1
+ *
+ * reverseList(list)
+ * start at the head
+ *
+ *
+ * current = head
+ * previous = head
+ *
+ * if current.next is null
+ *
+ *    current.next = previous
+ *
+ */
+
 function main() {
   let SLL = new LinkedList();
   SLL.insertLast('Apollo');
+  // SLL.insertLast('Apollo');
   SLL.insertLast('Boomer');
+  // SLL.insertLast('Boomer');
   SLL.insertLast('Hello');
   SLL.insertLast('Husker');
+  // SLL.insertLast('Husker');
   SLL.insertLast('Starbuck');
+  // SLL.insertLast('Starbuck');
   // console.log(SLL);
   SLL.insertLast('Tauhida');
   SLL.remove('squirrel');
   SLL.insertAt(0, 'x');
+  // console.log(size(SLL));
+  // console.log(isEmpty(SLL));
+  // console.log(findLast(SLL));
+
+  // console.log(SLL.toString());
+  // console.log(whatDoesThisProgramDo(SLL));
   console.log(SLL.toString());
-  console.log(size(SLL));
-  console.log(isEmpty(SLL));
-  console.log(findLast(SLL));
+  console.log(third(SLL));
 }
 
 main();
